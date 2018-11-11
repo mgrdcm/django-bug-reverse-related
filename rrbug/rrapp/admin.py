b9a9385652e1b74af27cb90360a208ce0fb9e84f
Dan Moore <dan@moore.cx>
2018-11-11 09:07:59 -0500

    Adding example model and admin to trigger the bug.

--- rrbug/rrapp/admin.py
+++ rrbug/rrapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Thing, Category
+
+
+class ThingAdmin(admin.ModelAdmin):
+    list_filter = [
+
+        # This does not work (/admin/rrapp/thing/)
+        ('categories', admin.RelatedOnlyFieldListFilter),
+
+        # This works (/admin/rrapp/thing/)
+        # 'categories',
+    ]
+
+
+admin.site.register(Category)
+admin.site.register(Thing, ThingAdmin)
--- rrbug/rrapp/models.py
+++ rrbug/rrapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Thing(models.Model):
+    pass
+
+
+class Category(models.Model):
+    things = models.ManyToManyField(
+        Thing,
+        blank=True,
+        related_name='categories',
+    )
